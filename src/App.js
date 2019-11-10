27625c6b0b0e5b0d1641e05e58f42fb5e6318491
Harshit <hs271095@gmail.com>
2019-11-10 18:31:18 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import $ from 'jquery';
+import Header from './Components/Header';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App"> 
+        <Header data={this.state.resumeData.main}/>
+      </div>
+    );
+  }
+}
+
+export default App;
